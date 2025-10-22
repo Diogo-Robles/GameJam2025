233c35ed2379b31c3dc25fcf87fa8142dd1e37d9
Rodrigo-P-Carvalho <rodrigopc6161@gmail.com>
2025-10-22 10:01:41 -0300

    UI Basica + Casa
    adicionado uma UI basica e uma casa

--- Restaurador-Florestal-GODOT/hotbar_Script.gd
+++ Restaurador-Florestal-GODOT/hotbar_Script.gd
@@ -1,11 +1,28 @@
-extends AnimatedSprite2D
+extends TextureRect
 
 @onready var PlayerNode = get_tree().get_first_node_in_group("player")
+var anim_texture: AnimatedTexture
+
+func _ready():
+	if self.texture is AnimatedTexture:
+		print("aaaaaaaa")
+		anim_texture = self.texture as AnimatedTexture
+		anim_texture.pause = true
+	else:
+		print("⚠️ A textura do próprio nó não é uma AnimatedTexture. Verifique o Inspector.")
 
 func _process(delta: float) -> void:
-	if PlayerNode.HoldingItem == "Axe":
-		self.play("Selected1")
-	elif PlayerNode.HoldingItem == "Water":
-		self.play("Selected2")
-	else:
-		self.play("Selected3")
+	
+	var frame_to_set = 0
+	match PlayerNode.HoldingItem:
+		"Axe":
+			frame_to_set = 1
+		"Water":
+			frame_to_set = 2
+		"Seed":
+			frame_to_set = 3
+			
+	trocar_frame_por_script(frame_to_set)
+
+func trocar_frame_por_script(novo_frame: int):
+	anim_texture.current_frame = novo_frame
